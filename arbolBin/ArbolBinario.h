af9e1742457040a6db5eb54bc70954ceae79ae8c
marshall <h-marshall.lee>
2022-11-14 20:41:30 -0500

    algo: implementacion del metodo insertar

--- arbolBin/ArbolBinario.h
+++ arbolBin/ArbolBinario.h
@@ -7,8 +7,8 @@ template <class T>
 class NodoArbol{
   private:
     T Info;
-    NodoArbol<T> HijoIzq;
-    NodoArbol<T> HijoDer;
+    NodoArbol<T> * HijoIzq;
+    NodoArbol<T> * HijoDer;
   public:
     NodoArbol();
     T RegresaInfo();
@@ -22,7 +22,7 @@ NodoArbol<T>::NodoArbol(){
 }
 
 template <class T>
-T NodoArbol<T>:RegresaInfo(){
+T NodoArbol<T>::RegresaInfo(){
   return Info;
 }
 
@@ -32,8 +32,15 @@ template <class T>
 class ArbolBinario{
   private:
     NodoArbol<T> * Raiz;
+    void InsertarArbol(NodoArbol<T> *&, T);
   public:
     ArbolBinario();
+    void InsertarArbol(T Dato){
+      InsertarArbol(Raiz, Dato);
+    }
+    T m(){
+      return Raiz->HijoDer->Info;
+    }
 };
 
 template <class T>
@@ -41,6 +48,20 @@ ArbolBinario<T>::ArbolBinario(){
   Raiz = nullptr;
 }
 
+template <class T>
+void ArbolBinario<T>::InsertarArbol(NodoArbol<T> *& Apunt, T Dato){
+  if(Apunt == nullptr){
+    NodoArbol<T> * aux = new NodoArbol<T>();
+    aux->Info = Dato;
+    Apunt = aux;
+  }
+  else{
+    if(Dato < Apunt->Info)
+      InsertarArbol(Apunt->HijoIzq, Dato);
+    else
+      InsertarArbol(Apunt->HijoDer, Dato);
+  }
+}
 
 #endif
 
--- arbolBin/main.cpp
+++ arbolBin/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "ArbolBinario.h"
 using namespace std;
 
 int main(){
+  ArbolBinario<int> *ab = new ArbolBinario<int>();
+  ab->InsertarArbol(2);
+  ab->InsertarArbol(3);
+  cout << ab->m() << endl;
   return 0;
 }
