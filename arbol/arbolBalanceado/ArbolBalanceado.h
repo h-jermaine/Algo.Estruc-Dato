e64d47a4af95502740e804228d42fe77dfc573eb
marshall <h-marshall.lee@hotmail.com>
2022-11-15 05:19:36 -0800

    prueba 1

--- arbol/arbolBalanceado/ArbolBalanceado.h
+++ arbol/arbolBalanceado/ArbolBalanceado.h
@@ -18,16 +18,55 @@ class NodoArbolBal{
 };
 
 template <class T>
+NodoArbolBal<T>::NodoArbolBal(){
+  HijoDer = nullptr;
+  HijoIzq = nullptr;
+}
+
+template <class T>
+T NodoArbolBal<T>::RegresaInfo(){
+  return Info;
+}
+
+template <class T>
+void NodoArbolBal<T>::ActulizaInfo(T Dato){
+  Info = Dato;
+}
+
+/*
+ *Declaracion de la clase ArbolBalanceado
+ * */
+
+template <class T>
 class ArbolBalanceado{
   private:
     NodoArbolBal<T> * Raiz;
   public:
     ArbolBalanceado();
-};
+    NodoArbolBal<T> * RegresaRaiz();
+    NodoArbolBal<T> * Busca(NodoArbolBal<T> *, T);
+    void InsertaBalanceado(T, NodoArbolBal<T> *, int *);
+    
+    NodoArbolBal<T> * RotacionHI_HD(NodoArbolBal<T> *, NodoArbolBal<T> *);
+    NodoArbolBal<T> * RotacionHD_HI(NodoArbolBal<T> *, NodoArbolBal<T> *);
+    NodoArbolBal<T> * RotacionHI_HI(NodoArbolBal<T> *, NodoArbolBal<T> *);
+    NodoArbolBal<T> * RotacionHD_HD(NodoArbolBal<T> *, NodoArbolBal<T> *);
+
+    /*NodoArbolBal<T> *RestructuraI(NodoArbolBal<T> *, int *);
+    NodoArbolBal<T> *RestructuraD(NodoArbolBal<T> *, int *);
+    void EliminaBalanceado(NodoArbolBal<T> *, NodoArbolBal<T> *, int *, T);
+    void Sustituye(NodoArbolBal<T> *, NodoArbolBal<T> *, int *);*/
+    void Imprime(NodoArbolBal<T> *);
+};	
 
 template <class T>
 ArbolBalanceado<T>::ArbolBalanceado(){
   Raiz = nullptr;
+}
+
+template <class T>
+NodoArbolBal<T> * ArbolBalanceado<T>::RegresaRaiz(){
+  return Raiz;
 }
 
 template <class T>
@@ -36,6 +75,31 @@ NodoArbolBal<T> * ArbolBalanceado<T>::RotacionHI_HI(NodoArbolBal<T> *Apunt, Nodo
   ApAux1->HijoDer = Apunt;
   Apunt->FE = 0;
   return ApAux1;
+}
+
+template <class T>
+NodoArbolBal<T> * ArbolBalanceado<T>::Busca(NodoArbolBal<T> * Apunt, T Dato){
+  if(Apunt != nullptr){
+    if(Apunt->Info == Dato)
+      return Apunt;
+    else{
+      if(Apunt->Info > Dato)
+	return Busca(Apunt->HijoIzq, Dato);
+      else
+	return Busca(Apunt->HijoDer, Dato);
+    }
+  }
+  else
+    return nullptr;
+}
+
+template <class T>
+void ArbolBalanceado<T>::Imprime(NodoArbolBal<T> * Apunt){
+  if(Apunt != nullptr){
+    Imprime(Apunt->HijoIzq);
+    std::cout << Apunt->Info << std::endl << std::endl;
+    Imprime(Apunt->HijoDer);
+  }
 }
 
 template <class T>
@@ -68,17 +132,109 @@ NodoArbolBal<T> * ArbolBalanceado<T>::RotacionHI_HD(NodoArbolBal<T> *Apunt, Nodo
 template <class T>
 NodoArbolBal<T> * ArbolBalanceado<T>::RotacionHD_HI(NodoArbolBal<T> * Apunt, NodoArbolBal<T> * ApAux1){
   NodoArbolBal<T> * ApAux2;
-  ApAux2 = ApAux1->HijoIzq;                       Apunt->HijoDer = ApAux2->HijoIzq;
-  ApAux2->HijoIzq = Apunt
-  ApAux1->HijoIzq = ApAux2->HijoDer;              ApAux2->HijoDer = ApAux1;                       if(ApAux2->FE == -1)
-    Apunt->FE = 1;
+  ApAux2 = ApAux1->HijoIzq;                       
+  Apunt->HijoDer = ApAux2->HijoIzq;
+  ApAux2->HijoIzq = Apunt;
+  ApAux1->HijoIzq = ApAux2->HijoDer;              
+  ApAux2->HijoDer = ApAux1;                       
+  if(ApAux2->FE == 1)
+    Apunt->FE = -1;
   else                                        
-    Apunt->FE = 0;                                if(ApAux2->FE == 1)                               Apunt->FE = -1;                               else                                              Apunt->FE = 0;                                return ApAux2;
+    Apunt->FE = 0;                                
+  if(ApAux2->FE == -1)                               
+    ApAux1->FE = 1;                               
+  else                                              
+    ApAux1->FE = 0;                                
+  return ApAux2;
 }
 
+template <class T>
+void ArbolBalanceado<T>::InsertaBalanceado(T Dato, NodoArbolBal<T> * Apunt, int *Band){
+  NodoArbolBal<T> * ApAux1, * ApAux2;
+  if(Apunt != nullptr){
+    if(Dato < Apunt->Info){
+      InsertaBalanceado(Dato, Apunt->HijoIzq, Band);
+      Apunt->HijoIzq = Raiz;
+      if(0 < *Band)
+	switch(Apunt->FE){
+	  case 1:{
+		   Apunt->FE = 0;
+		   *Band = 0;
+		   break;
+		 }
+	  case 0:{
+		   Apunt->FE = -1;
+		   break;
+		 }
+	  case -1:{
+		    ApAux1 = Apunt->HijoIzq;
+		    if(ApAux1->FE <= 0)
+		      Apunt = RotacionHI_HI(Apunt, ApAux1);
+		    else
+		      Apunt = RotacionHI_HD(Apunt, ApAux1);
+		    Apunt->FE = 0;
+		    *Band = 0;
+		  }
+	}
+    }
+    else{
+      if(Dato > Apunt->Info){
+	if(0 < *Band){
+	  switch(Apunt->FE){
+	    case -1:{
+		      Apunt->FE = 0;
+		      *Band = 0;
+		      break;
+		    }
+	    case 0:{
+		     Apunt->FE = 1;
+		     break;
+		   }
+	    case 1:{
+		     ApAux1 = Apunt->HijoDer;
+		     if(ApAux1->FE >= 0)
+		       Apunt = RotacionHD_HD(Apunt, ApAux1);
+		     else
+		       Apunt = RotacionHD_HI(Apunt, ApAux1);
+		     Apunt->FE = 0;
+		     *Band = 0;
+		   }
+
+	  }
+	}
+	Raiz = Apunt;
+      }
+      else{
+	ApAux2 = new NodoArbolBal<T>();
+	ApAux2->Info = Dato;
+	ApAux2->FE = 0;
+	*Band = 1;
+	Raiz = ApAux2;
+      }
+    }
+  }
+}
+
+// Eliminacion de arbolesBalanceados Pg. 371
 
 
 
 
 
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- arbol/arbolBalanceado/main.cpp
+++ arbol/arbolBalanceado/main.cpp
@@ -3,5 +3,13 @@
 using namespace std;
 
 int main(){
+  ArbolBalanceado<int> * a1 = new ArbolBalanceado<int>();
+  NodoArbolBal<int> * Apunt1;
+  int Band;
+  Apunt1 = a1->RegresaRaiz();
+  a1->InsertaBalanceado(11, Apunt1, &Band);
+  Apunt1 = a1->RegresaRaiz();
+  a1->Imprime(Apunt1);
+
   return 0;
 }
